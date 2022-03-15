491ae3f1232438f1a273c4935500d3e677bcde13
toyman <falakoadebayor@gmail.com>
2022-03-15 16:35:10 +0100

    my changes

--- humanR/models.py
+++ humanR/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+
+    employee_SN = models.CharField(max_length=100, verbose_name='Surname')
+    employee_MN = models.CharField(max_length=100, verbose_name='Middle Name')
+    employee_FN= models.CharField(max_length=100, verbose_name='First Name')
+    address = models.CharField(max_length=200, verbose_name='Address')
+    phone = models.IntegerField(max_length=50, verbose_name='Phone Number')
+    
+    
