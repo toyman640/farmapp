0a813175942714aad174292710fcc1f36c066b3a
toyman <falakoadebayor@gmail.com>
2022-05-30 14:29:35 +0100

    my changes

--- farmapp/settings.py
+++ farmapp/settings.py
@@ -21,6 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # STATIC_ROOT = '/home/skaazzna/public_html/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR,]
 
 
