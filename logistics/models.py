e2115397c38cf51c320317b1ebfdfa49b325cfdd
toyman640 <falakoadebayor@gmail.com>
2023-11-22 10:44:15 +0100

    made corrections

--- logistics/models.py
+++ logistics/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.text import slugify
-from autoslug import AutoSlugField
+# from autoslug import AutoSlugField
 
 # Create your models here.
 
