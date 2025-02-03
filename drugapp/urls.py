0c2e4fc7da6fa3d8e628751d935ef53c0ac388db
toyman640 <falakoadebayor@gmail.com>
2025-02-03 11:02:43 +0100

    Add form to add unit

--- drugapp/urls.py
+++ drugapp/urls.py
@@ -4,5 +4,7 @@ from drugapp import views
 app_name = 'drugapp'
 
 urlpatterns = [
-  path('drug-app-index', views.drug_index, name='drug_index')
+  path('drug-app-index', views.drug_index, name='drug_index'),
+  path('add-unit/', views.add_unit, name='add_unit'),
+  path('units/', views.list_units, name='list_units'),
 ]
--- drugapp/views.py
+++ drugapp/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Drug, Dispatch, Unit
@@ -6,23 +7,65 @@ from django.forms import formset_factory
 
 # Create your views here.
 def drug_index(request):
-  return render(request, 'drugapp/index.html')
+  unit_form = UnitForm()
+  units = Unit.objects.all()
+  return render(request, 'drugapp/index.html', {'unit_form': unit_form, 'units': units})
+
+# def add_unit(request):
+#     """ View to create a new Unit """
+#     if request.method == 'POST':
+#         form = UnitForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, "Unit added successfully!")
+#             return redirect('add_unit')
+#     else:
+#         form = UnitForm()
+    
+#     return render(request, 'inventory/add_unit.html', {'form': form})
+
+
+# def add_unit(request):
+#   """ View to create a new Unit via modal """
+#   if request.method == 'POST':
+#     form = UnitForm(request.POST)
+#     if form.is_valid():
+#       form.save()
+#       if request.headers.get('X-Requested-With') == 'XMLHttpRequest':  
+#         return JsonResponse({'success': True})
+#       messages.success(request, "Unit added successfully!")
+#       return redirect('home')
+#     else:
+#       if request.headers.get('X-Requested-With') == 'XMLHttpRequest':  
+#         return JsonResponse({'success': False, 'errors': form.errors})
+
+#   else:
+#     form = UnitForm()
+  
+#   return render(request, 'drupapp/index.html', {'form': form})
+
 
 def add_unit(request):
-    """ View to create a new Unit """
-    if request.method == 'POST':
-        form = UnitForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Unit added successfully!")
-            return redirect('add_unit')
+  if request.method == 'POST':
+    form = UnitForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return JsonResponse({"success": True})
     else:
-        form = UnitForm()
-    
-    return render(request, 'inventory/add_unit.html', {'form': form})
+      return JsonResponse({"success": False, "errors": form.errors})
+  return JsonResponse({"success": False, "message": "Invalid request"})
+
+
+def list_units(request):
+  units = Unit.objects.all()
+  return render(request, 'inventory/list_units.html', {'units': units})
+
+
+def view_unit(request, unit_id):
+  unit = get_object_or_404(Unit, id=unit_id)
+  return render(request, 'drugapp/drug-records.html', {'unit': unit})
 
 def add_drug(request):
-    """ View to create a new Drug """
     if request.method == 'POST':
         form = DrugForm(request.POST)
         if form.is_valid():
@@ -35,20 +78,19 @@ def add_drug(request):
     return render(request, 'inventory/add_drug.html', {'form': form})
 
 def dispatch_drug(request):
-    """ View to dispatch multiple drugs at once """
     DispatchFormSet = formset_factory(DispatchForm, extra=3)
 
     if request.method == 'POST':
         formset = DispatchFormSet(request.POST)
         if formset.is_valid():
             for form in formset:
-                if form.cleaned_data:  # Avoid empty forms
+                if form.cleaned_data:
                     dispatch = form.save(commit=False)
                     dispatch.dispatched_by = request.user
                     dispatch.save()
             messages.success(request, "Drugs dispatched successfully!")
             return redirect('dispatch_drug')
     else:
-        formset = DispatchFormSet()
+      formset = DispatchFormSet()
     
     return render(request, 'inventory/dispatch_drug.html', {'formset': formset})
