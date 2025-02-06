890aab31702c52065f1587df8cb9b546866661ca
toyman640 <falakoadebayor@gmail.com>
2025-02-06 11:35:59 +0100

    Try to correct adding inventory

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,11 @@ app_name = 'main'
 
 
 urlpatterns = [
-    path('test/index', views.main_index, name='main_index'),
+    path('landing/index', views.main_index, name='main_index'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('drug-records/', views.drugs_inventory, name='drugs_inventory'),
+    path('drug/<int:drug_id>/details', views.drug_detail, name='drug_detail'),
+    path('drug/edit/<int:drug_id>/', views.edit_drug, name='edit_drug'),
+    path('drug/<int:drug_id>/delete/', views.delete_drug, name='delete_drug'),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render,  get_object_or_404
 from django.urls import reverse_lazy
 from django.db.models import F
 from django.utils.timezone import localtime, now, localdate
@@ -7,6 +7,8 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from drugapp.models import Dispatch, Drug
+from django.core.paginator import Paginator
+from drugapp.forms import DrugForm, DispatchForm, UnitForm, DispatchEditForm, DispatchFilter
 
 
 class CustomLoginView(LoginView):
@@ -46,4 +48,39 @@ def main_index(request):
 
   return render(request, 'main/index.html', context)
 
+
+def drugs_inventory(request):
+    dispatch_records = Drug.objects.all()
+    paginator = Paginator(dispatch_records, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'main/drug-records.html', {'page_obj': page_obj})
+
+
+def drug_detail(request, drug_id):
+  drug = get_object_or_404(Drug, id=drug_id)
+  return render(request, 'main/drug-info.html', {'drug': drug})
+
+
+def edit_drug(request, drug_id):
+  drug = get_object_or_404(Drug, id=drug_id)
+  if request.method == 'POST':
+    form = DrugForm(request.POST, instance=drug)
+    if form.is_valid():
+      form.save()
+      return redirect('main:drugs_inventory')
+  else:
+    form = DrugForm(instance=drug)
+  return render(request, 'main/edit-drug.html', {'form': form, 'drug': drug})
+
+
+def delete_drug(request, drug_id):
+  drug = get_object_or_404(Drug, id=drug_id)
+
+  if request.method == 'POST':
+    drug.delete()
+    return redirect('main:drugs_inventory')
+
+  return render(request, 'drugapp/confirm_delete.html', {'drug': drug})
 
