a5b61a7a71fdef6c8c05bf116ee0476fbcaf619c
toyman640 <falakoadebayor@gmail.com>
2025-03-25 17:09:08 +0100

    Fix error

--- drugapp/views.py
+++ drugapp/views.py
@@ -144,7 +144,7 @@ def add_drug(request):
 def update_drug_quantity(request, drug_id):
   drug = get_object_or_404(Drug, id=drug_id)
 
-<<<<<<< HEAD
+
   if request.method == "POST":
     form = UpdateDrugQuantityForm(request.POST)
     if form.is_valid():
@@ -163,20 +163,7 @@ def update_drug_quantity(request, drug_id):
 
   else:
     form = UpdateDrugQuantityForm()
-=======
-    if request.method == "POST":
-        form = UpdateDrugQuantityForm(request.POST)
-        if form.is_valid():
-          added_quantity = form.cleaned_data["quantity"]
-          new_quantity = drug.quantity + added_quantity
 
-          drug.update_stock(new_quantity, request.user)
-
-          messages.success(request, "Stock updated successfully!")
-          return redirect("drugapp:drugs_list")
-    else:
-        form = UpdateDrugQuantityForm()
->>>>>>> rebuild
 
   return render(request, "drugapp/update-drug.html", {"form": form, "drug": drug})
 
