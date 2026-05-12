a21bf2b5bab890a64c759b2a525c1da2e73f9b1d
toyman640 <falakoadebayor@gmail.com>
2026-05-12 16:09:01 +0100

    Fix

--- static/js/custom.js
+++ static/js/custom.js
@@ -21,27 +21,3 @@ $('.counter').each(function () {
     });
 });
 
-$(document).on('click', '.view-record', function () {
-   
-
-    console.log("CLICK WORKING");
-
-    let animal = $(this).attr('data-animal');
-    let total = $(this).attr('data-total');
-    let date = $(this).attr('data-date');
-    let notes = $(this).attr('data-notes');
-    let created = $(this).attr('data-created');
-
-    console.log(animal);
-
-    $('#modal-animal').html(animal);
-
-    $('#modal-total').html(total);
-
-    $('#modal-date').html(date);
-
-    $('#modal-notes').html(notes ? notes : '-');
-
-    $('#modal-created').html(created);
-
-});
--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,4 @@
-// $(function() {
-// 	var totalExeTime = 2000;
 
-//     function count($this){
-//         var currentCount = parseInt($this.html(), 10),
-//         	maxCount = $this.data('count');
-        
-//         $this.html(++currentCount);
-//         if(currentCount !== maxCount){
-//         	var ratio = totalExeTime / maxCount;
-        
-//             setTimeout(function(){
-//             	count($this)
-//            	}, ratio);
-//         }
-//     }        
-//   $(".number").each(function() {
-//       $(this).data('count', parseInt($(this).html(), 10));
-//       $(this).html('0');
-//       count($(this));
-//   });
-// });
 
 $('.number').each(function () {
     $(this).prop('Counter',0).animate({
@@ -32,15 +11,5 @@ $('.number').each(function () {
         }
     });
 });
-
-// $(document).ready(function() {
-//     $('.select2-drug').select2({
-//         placeholder: "Search drug...",
-//         allowClear: true
-//     });
-
-//     // Hide the Select2 container (for testing)
-//     $('.select2-drug').next('.select2').css('display', 'none');
-// });
 
 
